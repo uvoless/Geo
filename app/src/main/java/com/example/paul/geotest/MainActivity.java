e8468a64931d6718e7c20f9a2ee26d70a2111c64
juiyingchiu <uvoless@gmail.com>
2018-09-05 20:31:07 +0800

    Initial commit

---
+++ app/src/main/java/com/example/paul/geotest/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.paul.geotest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.example.paul.geotest.CountrySection;
+import com.example.paul.geotest.GeoMapView;
+import com.example.paul.geotest.OnInitializedListener;
+import com.example.paul.geotest.SVGParser;
+
+
+public class MainActivity extends AppCompatActivity {
+    GeoMapView geoMapView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        geoMapView = (GeoMapView) findViewById(R.id.geoMap);
+        geoMapView.setOnInitializedListener(new OnInitializedListener() {
+            @Override
+            public void onInitialized(GeoMapView geoMapView) {
+                progressBar.setVisibility(View.INVISIBLE);
+                geoMapView.setCountryColor("US", "#00FF00");
+                geoMapView.setCountryColor("JP", "#FF0000");
+                geoMapView.setCountryColor("ES", "#FF0000");
+
+
+                geoMapView.refresh();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/paul/geotest/OnInitializedListener.java
@@ -0,0 +1,8 @@
+package com.example.paul.geotest;
+
+/**
+ * Created by takuma on 2015/07/21.
+ */
+public interface OnInitializedListener {
+    void onInitialized(GeoMapView geoMapView);
+}
